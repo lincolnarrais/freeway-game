39b5960cf9c28c5f89442d38dbe83aacdda104a5
Lincoln Arrais <lincoln_arrais@hotmail.com>
2023-07-30 15:38:54 -0300

    added a collide2d library to detect collision between car and actor; added cars to all lanes; the player now gets points for making it to the other side of the street

--- ator.js
+++ ator.js
@@ -1,7 +1,9 @@
 // código do ator
 
-let xAtor = 100;
+let xAtor = 85;
 let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor() {
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -14,5 +16,42 @@ function movimentaAtor() {
 
   if (keyIsDown(DOWN_ARROW)) {
     yAtor += 3;
+  }
+}
+
+function verificaColisao() {
+  // collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++) {
+    colisao = collideRectCircle(
+      xCarros[i],
+      yCarros[i],
+      comprimentoCarro,
+      alturaCarro,
+      xAtor,
+      yAtor,
+      15
+    );
+
+    if (colisao) {
+      voltaAtorPosicaoInicial();
+    }
+  }
+}
+
+function voltaAtorPosicaoInicial() {
+  yAtor = 366;
+}
+
+function incluiPontos() {
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60));
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto() {
+  if (yAtor < 15) {
+    meusPontos++;
+    voltaAtorPosicaoInicial();
   }
 }
